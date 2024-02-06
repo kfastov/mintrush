ddcf39f4073c5e098663338020ba51acd8716db9
Konstantin Fastov <kfastov@gmail.com>
2024-02-06 12:28:32 +0700

    first commit

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import { useBlock } from "@starknet-react/core";
+
+function App() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen gap-12">
+      <p className="mb-2 text-5xl">
+        [Coming soon]
+      </p>
+    </main>
+  );
+}
+
+export default App;
